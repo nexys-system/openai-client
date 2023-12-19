1206b4050c2a042ecb33b5f1982a323611851c87
John B <johan@nexys.ch>
2023-12-19 15:38:40 +0100

    update models

--- src/type.ts
+++ src/type.ts
@@ -1,13 +1,16 @@
-// https://platform.openai.com/docs/models/model-endpoint-compatibility
+// https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
 export type ChatGPTModel =
+  | "gpt-4-1106-preview"
+  | "gpt-4-vision-preview"
   | "gpt-4"
-  | "gpt-4-0613"
   | "gpt-4-32k"
+  | "gpt-4-0613"
   | "gpt-4-32k-0613"
+  | "gpt-3.5-turbo-1106"
   | "gpt-3.5-turbo"
   | "gpt-3.5-turbo-0613"
   | "gpt-3.5-turbo-16k"
-  | "gpt-3.5-turbo-16k-0613"; // "gpt-3.5-turbo" | "gpt-4";
+  | "gpt-3.5-turbo-16k-0613"; 
 
 export type PayloadFunctionType = "string" | "object";
 
